1e352ff4768ebdee38c09493aa107b39a58b6562
Kazuk Nakai <na.kazuki08@gmail.com>
2021-03-19 12:17:25 +0900

    home画面のviewは完成テストはまだ

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,33 @@
+import React, { VFC } from 'react';
+import styled from 'styled-components';
+import {AppBar, Toolbar} from '@material-ui/core'
+import Button from '@material-ui/core/Button'
+import { Link } from "react-router-dom";
+// images
+import MainLogo from '../images/logo.png';
+
+// // css
+const MainLogoImage = styled.img`
+  height: 2.5rem;
+`;
+
+const ButtonWrapper = styled.div`
+  margin: 0 0 0 auto;
+`;
+
+export const Header:VFC = () => {
+  return (
+    <AppBar position="fixed" color="inherit">
+      <Toolbar>
+        <Link to ={ '/' } >
+           <MainLogoImage src={MainLogo} alt="main logo" />
+        </Link>
+        <ButtonWrapper>
+          <Button variant="contained"  color="primary">
+            ログイン
+          </Button>
+        </ButtonWrapper>
+      </Toolbar>
+    </AppBar>
+  )
+}
