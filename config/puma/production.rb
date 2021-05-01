543440cd600eff5018912c66a4622a8e2f73bfa2
Kazuk Nakai <na.kazuki08@gmail.com>
2021-05-02 08:35:41 +0900

    デプロイ準備

---
+++ config/puma/production.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3001
+environment ENV['RACK_ENV'] || 'production'
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
+
+plugin :tmp_restart
+
+app_dir = File.expand_path('..', __dir__)
+bind "unix://#{app_dir}/tmp/sockets/puma.sock"
+pidfile "#{app_dir}/tmp/pids/puma.pid"
+state_path "#{app_dir}/tmp/pids/puma.state"
+stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
