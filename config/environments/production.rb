d87ff8be307ee6b3b2b8481cfb7722f348613738
Kazuk Nakai <na.kazuki08@gmail.com>
2021-05-20 06:26:42 +0900

    ドメインに変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -123,6 +123,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  host = '13.114.225.30'
+  host = 'short-diary.com'
   Rails.application.routes.default_url_options[:host] = host
 end
