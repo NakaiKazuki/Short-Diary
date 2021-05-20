952b8aaf552001743001b8c89d871526c3988bbb
Kazuk Nakai <na.kazuki08@gmail.com>
2021-05-20 23:08:40 +0900

    修正

--- production.sh
+++ production.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
-docker-compose -f docker-compose-prod.yml build
+git pll
+
+docker-compose -f docker-compose-prod.yml build --no-cache
 
 docker-compose -f docker-compose-prod.yml up -d
+
+docker rmi $(docker images -f "dangling=true" -q)
