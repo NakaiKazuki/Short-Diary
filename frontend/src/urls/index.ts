52a05d23a409fe497bb49f803f01534eaeaebb82
Kazuk Nakai <na.kazuki08@gmail.com>
2021-06-24 18:37:34 +0900

    修正

--- frontend/src/urls/index.ts
+++ frontend/src/urls/index.ts
@@ -4,7 +4,7 @@ const DEFAULT_API_LOCALHOST: string =
 export const home: string = `${DEFAULT_API_LOCALHOST}`;
 
 // User関連のurl
-const DEVISE_PATH: string = `${DEFAULT_API_LOCALHOST}auth`;
+const DEVISE_PATH: string = `${DEFAULT_API_LOCALHOST}/auth`;
 
 export const registration: string = DEVISE_PATH;
 export const signIn: string = `${DEVISE_PATH}/sign_in`;
