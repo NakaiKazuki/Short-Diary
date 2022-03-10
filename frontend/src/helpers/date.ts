3057d61356c5a4c8ffd12d8dccac1767cc506179
Kazuk Nakai <na.kazuki08@gmail.com>
2022-03-11 08:03:43 +0900

    修正

--- frontend/src/helpers/date.ts
+++ frontend/src/helpers/date.ts
@@ -1,1 +1,5 @@
-export const dateToday = () => new Date(Date.now() + 9 * 3600000).toISOString().replace(/\..*/, '+09:00').split("T")[0];
+export const dateToday = () =>
+  new Date(Date.now() + 9 * 3600000)
+    .toISOString()
+    .replace(/\..*/, "+09:00")
+    .split("T")[0];
