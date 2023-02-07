e1e3d27cad97160f4f86346f9bda05d45928c120
Nakai Kazuki <na.kazuki08@gmail.com>
2023-02-08 06:15:19 +0900

    dockerの未使用volume削除するように変更

--- production.sh
+++ production.sh
@@ -10,3 +10,5 @@ docker-compose -f docker-compose-prod.yml build
 docker-compose -f docker-compose-prod.yml up -d
 
 docker rmi $(docker images -f "dangling=true" -q)
+
+docker system prune -a --volumes
