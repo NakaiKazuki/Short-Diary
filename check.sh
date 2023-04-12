37d43933deceba71bf6076b72b48ce691e178868
NakaiKazuki <na.kazuki08@gmail.com>
2023-04-13 04:12:23 +0900

    修正

--- check.sh
+++ check.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 bundle exec rails_best_practices . &&
-bundle exec brakeman -6 -A -w1 --no-pager &&
+bundle exec brakeman -7 -A -w1 --no-pager &&
 bundle exec bundle-audit check --update &&
 bundle exec rubocop -a &&
 bundle exec rspec
